c2993d9b0ae2395d76cb2caf6e5abf8d341d6055
yhaddish <yghaddish@gmail.com>
2023-10-27 12:51:29 -0400

    Created a registerApi function

---
+++ src/util/ApiUtil.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080";
+const frameToken = (token) => `Bearer ${token}`;
+
+const frameResponse = (
+  reqStatus = 0,
+  reqPayLoad = "Invalid request. Please try again later."
+) => {
+  return {
+    status: reqStatus,
+    payLoad: reqPayLoad,
+  };
+};
+
+export const registerApi = async (
+  username,
+  password,
+  emailId,
+  firstName,
+  lastName,
+  phone
+) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/user/signup`;
+    const apiResponse = await axios.post(url, {
+      username,
+      password,
+      emailId,
+      firstName,
+      lastName,
+      phone,
+    });
+    if (apiResponse.status === 200) {
+      response = frameResponse(1);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
