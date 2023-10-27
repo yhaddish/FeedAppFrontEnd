65a7b3baab25e180749dc04b9a27646b787597dc
yhaddish <yghaddish@gmail.com>
2023-10-27 11:45:47 -0400

    Created a from field component using Formik

--- src/context/applicationContext.js
+++ src/context/applicationContext.js
@@ -1,44 +1,55 @@
-import React, { useState } from "react";
-import { useCookies } from "react-cookie";
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
 
+//create a context object
 const AppContext = React.createContext();
 
+//Functional Arrow component 
 const AppContextProvider = ({ children }) => {
-  const [cookies, setCookie, removeCookie] = useCookies(["appToken"]);
-  const [userSessionData, setUserSessionData] = useState(undefined);
 
-  const setSession = (token) => {
-    setCookie("appToken", token, {
-      path: "/",
-      maxAge: 900, //15minutes
-    });
-  };
+    //setCookie function will allow us to update the value of "appToken" cookie
+    //removeCookie will allow is to delete the "appToken" from the browser
+    const [cookies, setCookie, removeCookie] = useCookies(["appToken"]);
+    const [userSessionData, setUserSessionData] = useState(undefined);
 
-  const getSession = () => {
-    const token = cookies.appToken || null;
-    return token;
-  };
+    //function which saves the token into cookies "appToken"=cookieName, value=token
+    const setSession = (token) => {
+        setCookie("appToken", token, {
+            path: "/",
+            maxAge: 900, //15minutes // change this to 24 hours(in seconds) if needed
+        });
+    };
 
-  const setUserData = (userData) => setUserSessionData(userData);
-  const getUserData = () => userSessionData;
+    //function allows us to retrieve the value of the token from the cookie by using cookies.appToken
+    const getSession = () => {
+        const token = cookies.appToken || null;
+        return token;
+    };
+    //setUserSessionData function is used to update the value of the userSessionData state variable 
+    //to the value of the userData argument.
+    const setUserData = (userData) => setUserSessionData(userData);
 
-  const logout = () => {
-    removeCookie("appToken", { path: "/" });
-    setUserData(undefined);
-  };
-  return 
-    <AppContext.Provider
-      value={{
+    //function returns the current value of the userSessionData state variable.
+    const getUserData = () => userSessionData;
+
+    //logout function which is being called when the user clicks on the logout button
+    const logout = () => {
+        removeCookie("appToken", { path: "/" });
+        setUserData(undefined);
+    };
+
+    return <AppContext.Provider value={{
         setSession,
         getSession,
         setUserData,
         getUserData,
         logout,
-      }}
+    }}
     >
-      {children}
+        {/* Provider make values available to children*/}
+        {children}
     </AppContext.Provider>
+}
 
-};
 export { AppContext };
 export default AppContextProvider;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppContextProvider from "./context/applicationContext";
-
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./appRoutes/AppRoutes";
+import AppContextProvider from "./context/applicationContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
