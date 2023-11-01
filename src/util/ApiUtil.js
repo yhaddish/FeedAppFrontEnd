08c797a85fa7dc3ab02de547d5ba8220cd8bb2bb
yhaddish <yghaddish@gmail.com>
2023-10-31 21:00:52 -0400

    sessionApi function added

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -130,3 +130,24 @@ export const resetPasswordApi = async (token, password) => {
     return response;
   }
 };
+
+export const sessionApi = async (token) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/user/get`;
+    const apiResponse = await axios.get(url, {
+      headers: { Authorization: frameToken(token) },
+    });
+    if (apiResponse.status === 200) {
+      response = frameResponse(1, apiResponse.data);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
