ecbaf870eb00847a4a4434ed6ff713c50d86639d
yhaddish <yghaddish@gmail.com>
2023-10-31 22:46:00 -0400

    Created a functionally complete profile component

--- src/pages/App/CompleteProfile.js
+++ src/pages/App/CompleteProfile.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UpdatePublicProfile from "../../components/UpdatePublicProfile";
+import { AppContext } from "../../context/applicationContext";
 
 const CompleteProfile = () => {
-  return <>Complete Profile Page</>;
+  const navigate = useNavigate();
+  const appContext = useContext(AppContext);
+  const userData = appContext.getUserData();
+
+  useEffect(() => {
+    document.title = "Complete your profile | Feed App";
+  }, []);
+
+  useEffect(() => {
+    if (userData && userData.profile) {
+      navigate("/app/dashboard");
+    }
+  }, [userData]);
+
+  return (
+    <main className="grid grid-cols-1 lg:grid-cols-1 gap-6 my-12 md:mx-12 w-2xl container px-2 mx-auto">
+      <UpdatePublicProfile />
+    </main>
+  );
 };
 
 export default CompleteProfile;
