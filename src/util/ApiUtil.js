953afc194310808954447b81c30f637bc93a5aa8
yhaddish <yghaddish@gmail.com>
2023-11-01 15:13:46 -0400

    Created a  addFeedMetaDataApi function

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -234,3 +234,29 @@ export const addFeedApi = async (token, content, picture) => {
     return response;
   }
 };
+
+export const addFeedMetaDataApi = async (token, feedId, isLike, comment) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/feeds/meta/${feedId}`;
+    const apiResponse = await axios.post(
+      url,
+      {
+        isLike,
+        comment,
+      },
+      { headers: { Authorization: frameToken(token) } }
+    );
+    if (apiResponse.status === 200) {
+      response = frameResponse(1);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
