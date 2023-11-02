f97fb86f79d7f5ac0256b5e3895fef58510dd96d
yhaddish <yghaddish@gmail.com>
2023-11-02 08:49:09 -0400

    Created a function called getMyFeedsApi

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -260,3 +260,24 @@ export const addFeedMetaDataApi = async (token, feedId, isLike, comment) => {
     return response;
   }
 };
+
+export const getMyFeedsApi = async (token, pageNumber) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/feeds/user/${pageNumber}/5`;
+    const apiResponse = await axios.get(url, {
+      headers: { Authorization: frameToken(token) },
+    });
+    if (apiResponse.status === 200) {
+      response = frameResponse(1, apiResponse.data);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
